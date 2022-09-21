7b4c6b265f612d52bdc8002d50ff74d0b27b5917
Niki van Driel <NikiD@zorgenzekerheid.nl>
2022-09-21 17:16:23 +0200

    feat(memory-game): duplicate and shuffle the cards

--- js/memory.js
+++ js/memory.js
@@ -1,5 +1,16 @@
 "use strict";
 
+// Deel 2
+class Card {
+    constructor(card1, card2=card1, set=card1, sound=card1) {
+        this.card1 = card1,
+        this.card2 = card2,
+        this.set = set,
+        this.sound = sound
+    }
+} 
+
+
 // maak een const myField en koppel deze aan de div met id="field"
 const myField = document.getElementById('field');
 
@@ -7,21 +18,51 @@ const myField = document.getElementById('field');
 myField.addEventListener('click', onClickCard);
 
 // de volgende regel krijg je vast cadeau:
-const myCardSet = ["duck", "kitten", "piglet", "puppy", "calf", "veal", "lamb", "rooster", "horse", "mouse", "dog", "cat", "goose", "goat", "sheep", "pig", "cow", "chick", "hen"];
+const myCardArray = ["duck", "kitten", "piglet", "puppy", "calf", "veal", "lamb", "rooster", "horse", "mouse", "dog", "cat", "goose", "goat", "sheep", "pig", "cow", "chick", "hen"];
+
+
+// verdubbel het aantal kaarten
+let myDbCardArray = myCardArray;
+
+myDbCardArray = myDbCardArray.concat(myCardArray);
+myDbCardArray = shuffle(myDbCardArray);
+
+const myCardSet = myDbCardArray.map(card => new Card(card));
+console.log(myCardSet)
+
+
+// schud de kaarten met Fisher–Yates
+function shuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+}
 
 // geef het document een event listener die bij het laden van de pagina de functie populateField aanroept
 window.addEventListener('load', populateField());
 
 // schrijf hier de nieuwe functie populateField()
 function populateField() {
-    // maak binnen deze nieuwe functie een for loop waarin over de array myCardSet geïtereerd wordt (Mag ook met forEach, dat is zelfs beter). 
+    // maak binnen deze nieuwe functie een for loop waarin over de array myCardArray geïtereerd wordt (Mag ook met forEach, dat is zelfs beter). 
     myCardSet.forEach(element => {
         let newTile = document.createElement('div');
         let newCard = document.createElement('img');
-        let imgURL = 'img/' + element + '.jpg';
+        let imgURL = 'img/' + element.card1 + '.jpg';
         newTile.setAttribute('class', 'board6');
         newCard.setAttribute('src', imgURL);
-        newCard.setAttribute('name', element);
+        newCard.setAttribute('name', element.card1);
         
         newTile.appendChild(newCard);
         myField.appendChild(newTile);
@@ -44,3 +85,4 @@ function onClickCard(e) {
 }
 // Zorg er voor dat als deze functie wordt uitgevoerd, m.b.v. de method getAttribute() in het console de naam van het aangeklikte dier getoond wordt.
 // Hint, je moet hierbij gebruik maken van de verwijzing e.target omdat je de naam van het element wilt weten waar op geklikt is, dus die het 'click' event verstuurd heeft.
+
